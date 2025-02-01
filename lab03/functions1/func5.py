c4b1b75d1b63cccb4b156b0e932b64d7e4c37d8d
altynaysailau <al_sailau@kbtu.kz>
2025-02-01 19:25:28 +0500

    week3

---
+++ lab03/functions1/func5.py
@@ -0,0 +1,10 @@
+
+def permutations(some):
+    n = len(some)
+
+    for i in range(n):
+        for j in range(n):
+            print(some[(j-i)], end=" ")
+        print()
+k=str(input("soz:"))
+permutations(k)
