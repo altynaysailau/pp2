3da829875c9aec7b026d9e65233e867501525e8d
altynaysailau <al_sailau@kbtu.kz>
2025-02-17 09:17:11 +0500

    week04

--- lab04/json/1.py
+++ lab04/json/1.py
@@ -1,27 +1,18 @@
 
-import json
+import json 
 
 
- 
-with open('/Users/altynaysailau/Desktop/Github/pp2/lab04/json/sample-data.json', 'r') as my_file:
-    data = json.load(my_file) 
-print('Interface Status')
-print('=' * 78)
+print("Interface status")
+print("================================================================================")
+print("DN                                                 Description           Speed    MTU  ")
+print("-------------------------------------------------- --------------------  ------  ------")
 
-DN = "DN"
-Description = 'Description'
-Speed = 'Speed'
-MTU = 'MTU'
+with open('json/sample-data.json') as f:
+    data = json.load(f)
 
-print(f"{DN:<50} {Description:<20}  {Speed:<8} {MTU:<5}") 
-print('-' * 50 + " " + '-' * 20 + "  " +  '-' * 6 + "  " +  '-' * 6)
-
-
-for item in data['imdata']: 
-    attributes = item['l1PhysIf']['attributes'] 
-   
-    dn = attributes.get('dn') 
-    description = attributes.get('descr', '') 
-    speed = attributes.get('speed', 'inherit') 
-    mtu = attributes.get('mtu', '')  
-    print(f"{dn:<50} {description:<20} {speed:<9} {mtu:<5}")
+request_from_server = data["imdata"][0]["l1PhysIf"]["attributes"]["dn"]
+speed = data["imdata"][0]["l1PhysIf"]["attributes"]["fecMode"]
+mtu = data["imdata"][0]["l1PhysIf"]["attributes"]["mtu"]
+print(request_from_server, "                            ", speed," ", mtu )
+print(request_from_server, "                            ", speed," ", mtu )
+print(request_from_server, "                            ", speed," ", mtu )
