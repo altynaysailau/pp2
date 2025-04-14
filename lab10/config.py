9d7af85b688ede5fc8446a7f0e630e597537e951
altynaysailau <al_sailau@kbtu.kz>
2025-04-14 10:45:50 +0500

    week10

---
+++ lab10/config.py
@@ -0,0 +1,5 @@
+host = "localhost"         # или IP-адрес сервера
+user = "postgres"          # имя пользователя БД
+password = "your_password" # пароль от БД
+db_name = "your_db_name"   # название БД
+port = 5432                # порт PostgreSQL
